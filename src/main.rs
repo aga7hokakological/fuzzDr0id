ff014b9fbb86f224d8295bf881e650c87c486d17
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2020-06-30 23:46:40 +0530

    sample commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate rand;
+
+use rand::{thread_rng, Rng};
+
+fn main() {
+	let population = "<({[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]})> '~`!@#$%^&*_+-=|/?";
+	//Sample 
+	let target = "to be or not to be that is the question.!";
+}
